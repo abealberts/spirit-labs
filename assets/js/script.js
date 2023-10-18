835392706abedcf03be934d154d2abd2bc2630c5
Abe <abe.wav.bis@gmail.com>
2023-10-18 18:45:07 -0500

    Updated JS, Fixed conflicts

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,13 @@ $(function () {
     var requestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a';
     var resultsArr = [];
 
+    //DOES NOT WORK RN
     function getApi(requestUrlArg) {
         fetch(requestUrlArg)
         .then(function (response) {
             var data = response.json();
             console.log(data);
 
-
-            //DOES NOT WORK
             for (let i = 0; i < data.length; i++) {
                 resultsArr.push(data.drinks[i]);
             }
