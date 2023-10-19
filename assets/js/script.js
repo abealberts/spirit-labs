5d79cd4cd936c2d1a52a4c7778e679e34db402ef
Abe <abe.wav.bis@gmail.com>
2023-10-18 21:05:23 -0500

    Updated HTML, Some psuedocode in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 const apiKey = "9973533"
-const requestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a';
+const requestUrl = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?appid=9973533&f=a';
 var resultsArr = [];
 
 
-async function getApi(){
+async function searchDrinks(){
     const response = await fetch(requestUrl)
     var data = await response.json();
 
@@ -12,4 +12,19 @@ async function getApi(){
     console.log(resultsArr);
 }
 
-getApi(requestUrl);
+searchDrinks();
+
+//SOME PSUEDO CODE FOR SEARCH FUNCTIONALITY
+// $("#searchButton").click(function(){
+//     Build api link based on each dropdown
+
+//     endpoint url + searchAlcoholic + Liquor + etc
+
+//     if ($("#searchAlcoholic").val()){
+//         If theres a value for this dropdown, add to url
+//     }
+
+//     if ($("#searchAlcoholic").val() = No){
+//         searchLiquor = disabled
+//     }
+// })
