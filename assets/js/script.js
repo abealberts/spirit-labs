ae9d82967e7c7cef9037238a336bfd8579a7a5f2
DeAngelo Jones <deangelojones0708@gmail.com>
2023-10-24 18:51:50 -0500

    added local storage to getDOTD function

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,45 +23,77 @@ async function getRandomDrink() {
     randomArr = data.drinks;
 }
 
-async function fetchFavoritesData(){
+async function fetchFavoritesData() {
     const response = await fetch("https://www.thecocktaildb.com/api/json/v2/9973533/search.php" + "?i=" + drinkId)
     var data = await response.json();
+    console.log(data);
 }
 
 async function getDOTD() {
-    // fetch gets a random drink from the api
+    // Fetch a random drink from the api
     const response = await fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
-    var data = await response.json();
+    const data = await response.json();
+    const dotdArr = data.drinks;
+    console.log(dotdArr);
 
-    dotdArr = data.drinks
+    // Adds Drink of the Day data to page
+    $("#dotdImage").attr("src", dotdArr[0].strDrinkThumb);
+    $("#dotdName").text(dotdArr[0].strDrink);
+    $("#dotdIngredient1").text(dotdArr[0].strIngredient1);
+    $("#dotdIngredient2").text(dotdArr[0].strIngredient2);
+    $("#dotdIngredient3").text(dotdArr[0].strIngredient3);
+    $("#dotdIngredient4").text(dotdArr[0].strIngredient4);
+    $("#dotdIngredient5").text(dotdArr[0].strIngredient5);
+    $("#dotdIngredient6").text(dotdArr[0].strIngredient6);
+    $("#dotdIngredient7").text(dotdArr[0].strIngredient7);
 
-    $(document).ready(function(){
-    
-        
-        function updateTime () {
-        
-        const currentIndex = 0;
-
-            $("#dotdImage").attr("src", dotdArr[currentIndex].strDrinkThumb),
-            $("#dotdName").text(dotdArr[currentIndex].strDrink),
-            $("#dotdIngredient1").text(dotdArr[currentIndex].strIngredient1),
-            $("#dotdIngredient2").text(dotdArr[currentIndex].strIngredient2),
-            $("#dotdIngredient3").text(dotdArr[currentIndex].strIngredient3),
-            $("#dotdIngredient4").text(dotdArr[currentIndex].strIngredient4),
-            $("#dotdIngredient5").text(dotdArr[currentIndex].strIngredient5),
-            $("#dotdIngredient6").text(dotdArr[currentIndex].strIngredient6),
-            $("#dotdIngredient7").text(dotdArr[currentIndex].strIngredient7)
-
-            // increment the index or reset to 0 if it exceeds the array length
-            currentIndex = (currentIndex + 1) % dotdArr.length;
-
-            // set the next update to start after 5 seconds
-            setTimeout(updateTime, 5000);
-        }
-        // initial content change
-        updateTime();
-    })
 }
+updateDOTDDaily()
+
+function updateDOTDDaily() {
+    var updateInterval = 10 * 1000;
+    var lastUpdate = localStorage.getItem("dotdLastUpdate")
+
+    // Updates time for the Drink of the Day
+    if (!lastUpdate || dayjs().diff(dayjs(lastUpdate)) >= updateInterval) {
+        getDOTD();
+        localStorage.setItem("dotdLastUpdate", dayjs().format());
+
+        // If it is not time to update, the existing data will remain on the page
+    } else {
+
+        getExistingDOTD();
+    }
+    // Schedule's the next update
+    setTimeout(updateDOTDDaily, updateInterval);
+
+}
+
+function getExistingDOTD() {
+
+    // Retreives the data from local storage
+    const dotdImage = localStorage.getItem("dotdImage");
+    const dotdName = localStorage.getItem("dotdName");
+    const dotdIngredient1 = localStorage.getItem("dotdIngredient1");
+    const dotdIngredient2 = localStorage.getItem("dotdIngredient2");
+    const dotdIngredient3 = localStorage.getItem("dotdIngredient3");
+    const dotdIngredient4 = localStorage.getItem("dotdIngredient4");
+    const dotdIngredient5 = localStorage.getItem("dotdIngredient5");
+    const dotdIngredient6 = localStorage.getItem("dotdIngredient6");
+    const dotdIngredient7 = localStorage.getItem("dotdIngredient7");
+
+    // Updates the HTML with the stored data 
+    $("#dotdImage").attr(dotdImage);
+    $("#dotdName").text(dotdName);
+    $("#dotdIngredient1").text(dotdIngredient1);
+    $("#dotdIngredient2").text(dotdIngredient2);
+    $("#dotdIngredient3").text(dotdIngredient3);
+    $("#dotdIngredient4").text(dotdIngredient4);
+    $("#dotdIngredient5").text(dotdIngredient5);
+    $("#dotdIngredient6").text(dotdIngredient6);
+    $("#dotdIngredient7").text(dotdIngredient7);
+}
+
 
 $("#randButton").click(async function () {
     await getRandomDrink();
@@ -78,12 +110,12 @@ $("#randButton").click(async function () {
     console.log(rand);
 });
 
-$("#randFavoriteBtn").click(function(){
+$("#randFavoriteBtn").click(function () {
     drinkName = $(this).parents(".box").children("#randName").text();
     console.log(drinkName);
 });
 
-$("#serveDrinks").click(async function(){
+$("#serveDrinks").click(async function () {
 
     var liquor = $("#liquor").val();
     var ingredient1 = $("#ingredient1").val();
@@ -95,23 +127,23 @@ $("#serveDrinks").click(async function(){
     $("#searchError").hide();
 
     //Formats ingredients for URL
-    if (ingredient1 && ingredient2){
+    if (ingredient1 && ingredient2) {
         ingredientsUrl = ingredient1 + "," + ingredient2;
-    } else if (ingredient1 && !ingredient2){
+    } else if (ingredient1 && !ingredient2) {
         ingredientsUrl = ingredient1;
-    } else if (!ingredient1 && ingredient2){
+    } else if (!ingredient1 && ingredient2) {
         ingredientsUrl = ingredient2;
     };
 
     //Formats Liquor for URL
-    if (liquor && ingredientsUrl){
+    if (liquor && ingredientsUrl) {
         liquorUrl = liquor + ",";
-    } else if (liquor && !ingredientsUrl){
+    } else if (liquor && !ingredientsUrl) {
         liquorUrl = liquor;
     };
 
     //Send API Request with the selected ingredients assuming at least one selection is made
-    if (!liquor && !ingredient1 && !ingredient2){
+    if (!liquor && !ingredient1 && !ingredient2) {
         $("#searchError").show();
     } else {
         searchUrl = requestUrl + "i=" + liquorUrl + ingredientsUrl;
@@ -120,12 +152,13 @@ $("#serveDrinks").click(async function(){
 
         const response = await fetch(searchUrl)
         var data = await response.json();
-    
+
         resultsArr = data.drinks;
-    
+
         console.log(data);
-    };    
-;})
+    };
+    ;
+})
+
 
 searchDrinks()
-getDOTD();
